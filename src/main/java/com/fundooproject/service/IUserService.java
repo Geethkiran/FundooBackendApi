5ff00f1bee9459952bfb1bdb10fa4f4735024e5d
Geethkiran <geeth53xy@gmail.com>
2020-05-07 19:04:37 +0530

    [geeth]Add. abstraction is provided for the methods defined in userServiceImplementation class

--- src/main/java/com/fundooproject/service/IUserService.java
+++ src/main/java/com/fundooproject/service/IUserService.java
@@ -3,7 +3,6 @@ package com.fundooproject.service;
 import com.fundooproject.dto.LoginDto;
 import com.fundooproject.dto.UserDto;
 import com.fundooproject.model.User;
-
 import java.util.List;
 
 public interface IUserService {
